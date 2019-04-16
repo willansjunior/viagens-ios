52cbc6f88ed88a4df315ca589df623e83b77db19
Willans Junior <willans.junior@mv.com.br>
2019-04-15 22:44:23 -0300

    Implementacao da lista de collections e inicio da tela de detalhe de pacotes.

--- viagens/viagens/controller/PacoteViagemViewController.swift
+++ viagens/viagens/controller/PacoteViagemViewController.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
-class PacoteViagemViewController: UIViewController, UICollectionViewDataSource {
+class PacoteViagemViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate, UICollectionViewDelegate {
 
     @IBOutlet weak var collectionPacoteViagem: UICollectionView!
+    @IBOutlet weak var searchViagens: UISearchBar!
+    @IBOutlet weak var labelQuantidadePacotes: UILabel!
     
-    let list:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    let listaCompleta = ViagemDAO().retornaTodasAsViagens()
+    var list:Array<Viagem> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Preenchendo lista
+        list = listaCompleta
         
         //Informar ao view controller que ele implementa as funcionalidade do dataSource
         collectionPacoteViagem.dataSource = self
+        
+        //Informar ao controller que ele ira implementar o delegate
+        collectionPacoteViagem.delegate = self
+        
+        //Informar ao controller que ele ira implementar o searchDelegate
+        searchViagens.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -48,16 +59,40 @@ class PacoteViagemViewController: UIViewController, UICollectionViewDataSource {
         
         return celulaPacote
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let tamanhoCelula = collectionView.bounds.width / 2
+        return CGSize(width: tamanhoCelula - 15, height: 160)
     }
-    */
+    
+    //Função para clique de um item da collection
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //Recupera o storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        //Recupera a view a partir do storyboard
+        let controller = storyboard.instantiateViewController(withIdentifier: "detalhe") as! DetalhePacoteViewController
+        
+        //Carrega a view
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        list = listaCompleta
+        
+        //Filtando a lista
+        var pacotes = list
+        if searchText != "" {
+            pacotes = pacotes.filter({ $0.titulo.contains(searchText) })
+            list = pacotes
+        }
+        self.labelQuantidadePacotes.text = atualizaContagem()
+        collectionPacoteViagem.reloadData()
+    }
 
+    //Atualizador do contador da label da lista de pacotes
+    func atualizaContagem() -> String {
+        return list.count == 1 ? "1 pacote encontrado" : "\(list.count) pacotes encontrados"
+    }
+    
 }
